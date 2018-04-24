be1c7377a167f652600ba318d20d2c79e39ff0bd
Joseph Sams <joe.mac@Josephs-iMac.local>
2018-04-24 19:33:59 -0400

    Fixed bug with erroring when there were no orders returned from search

--- src/components/Orders/Results.jsx
+++ src/components/Orders/Results.jsx
@@ -9,37 +9,40 @@ class Results extends Component {
   state = { orders: [], loading: true };
 
   componentWillMount() {
-    api.search.byNameDate(this.props.query).then(orders => this.setState({ orders, loading: false }));
+    api.search
+      .byNameDate(this.props.query)
+      .then(orders => this.setState({ orders, loading: false }))
+      .catch(() => {});
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.query !== this.props.query) {
       this.setState({ loading: true });
-      api.search.byNameDate(this.props.query).then(orders => this.setState({ orders, loading: false }));
+      api.search
+        .byNameDate(this.props.query)
+        .then(orders => this.setState({ orders, loading: false }))
+        .catch(() => {});
     }
   }
 
   render() {
-    const orderResults = this.state.orders.map(order => (
-      <div key={order.id}>
-        <IconButton component={Link} to={`/order/${order.id + 100000}`}>
-          <ZoomIn />
-        </IconButton>
-        <Link style={{ marginLeft: '10px' }} to={`/order/${order.id + 100000}`}>
-          {order.id + 100000}
-        </Link>
-      </div>
-    ));
+    const orderResults =
+      this.state.orders.length > 0 ? (
+        this.state.orders.map(order => (
+          <div key={order.id}>
+            <IconButton component={Link} to={`/order/${order.id + 100000}`}>
+              <ZoomIn />
+            </IconButton>
+            <Link style={{ marginLeft: '10px' }} to={`/order/${order.id + 100000}`}>
+              {order.id + 100000}
+            </Link>
+          </div>
+        ))
+      ) : (
+        <div style={{ margin: '15px 0' }}>No match</div>
+      );
 
-    return (
-      <div>
-        {this.state.loading ? (
-          <CircularProgress />
-        ) : (
-          <div>{this.state.orders.length === 0 ? 'No match' : orderResults}</div>
-        )}
-      </div>
-    );
+    return <div>{this.state.loading ? <CircularProgress /> : <div>{orderResults}</div>}</div>;
   }
 }
 
